d3aa9a47628a8b203f77364a94459532229d866d
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-02-05 10:27:46 -0500

    added usercontroller actions

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
+// Importing the 'express' framework for building the web application
 import express from "express";
+// Importing the 'dotenv' library for handling environment variables
 import dotenv from "dotenv";
+// Importing setup functions for MongoDB, routes, and Passport authentication
 import MongooseSetup from "./lib/MongooseSetup.js";
 import RoutesSetup from "./lib/RoutesSetup.js";
 import PassportSetup from "./lib/PassportSetup.js";
+// Importing the 'express-session' middleware for session management
+import session from "express-session";
 
+// Loading environment variables from the '.env' file
 dotenv.config();
 
+// Setting up MongoDB connection using the imported function
 MongooseSetup();
 
+// Creating an instance of the express application
 const app = express();
 
-//call passport setup method for project auth
+// Configuring session middleware for managing user sessions and adding secret key for user encryption
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+    },
+  })
+);
+
+// Calling Passport setup method to configure authentication
 PassportSetup(app);
 
-//set ejs, view engine and other configurations
+// Setting EJS as the view engine and configuring other application settings
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//use middleware, in this case ...
+// Using middleware to handle the '_method' property for HTTP method override
 app.use((req, _, next) => {
   if (req.body && typeof req.body === "object" && "_method" in req.body) {
     const method = req.body._method;
@@ -32,19 +54,25 @@ app.use((req, _, next) => {
   next();
 });
 
+// Setting up routes using the imported function
 RoutesSetup(app);
 
-// Error handler middleware
+// Error handler middleware to handle and log errors
 app.use((error, _, res, __) => {
+  // If the error is a string, convert it to an Error object
   if (typeof error === "string") {
     error = new Error(error);
   }
 
+  // If the error has no status, set it to 404 (Not Found)
   if (!error.status) error.status = 404;
 
+  // Logging the error to the console
   console.error(error);
 
+  // Sending the appropriate HTTP status and error message in the response
   res.status(error.status).send(error.message);
 });
 
+// Exporting the configured express application
 export default app;
---
+++ controllers/UserController.js
@@ -0,0 +1,188 @@
+import { error } from "console";
+import User from "../models/User";
+import fs, { copyFileSync } from "fs";
+
+//helper function to find and verify user
+async function findAndVerifyUser(req) {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    req.status = 404;
+    throw new Error("User does not exist");
+  }
+
+  return user;
+}
+
+// Helper function to extract and filter specific parameters from the request body.
+// If a file is attached to the request, it assigns it to the 'avatar' property.
+function getStrongParams(req) {
+  // Check if a file is attached to the request
+  if (req.file) {
+    // If a file exists, assign it to the 'avatar' property in the request body
+    req.body.avatar = req.file;
+  }
+
+  // Destructure specific properties from the request body
+  const { id, firstName, lastName, nickname, email, avatar, password } =
+    req.body;
+
+  // Return an object containing the extracted properties
+  return { id, firstName, lastName, nickname, email, avatar, password };
+}
+
+export const index = async (req, res, next) => {
+  try {
+    const users = await User.find();
+
+    res.render("users/index", {
+      title: "List of Users",
+      users,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const show = async (req, res, next) => {
+  try {
+    const user = await findAndVerifyUser(req);
+
+    res.render("users/show", {
+      user,
+      title: "User View",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const add = async (req, res, next) => {
+  try {
+    res.render("users/add", {
+      formType: "create",
+      title: "New User",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const edit = async (req, res, next) => {
+  try {
+    const user = await findAndVerifyUser(req);
+
+    res.render("users/edit", {
+      user,
+      formType: "update",
+      title: "Edit User",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const create = async (req, res, next) => {
+  try {
+    const { firstName, lastName, nickname, email, password, avatar } =
+      getStrongParams(req);
+    const user = new User({
+      firstName,
+      lastName,
+      nickname,
+      email,
+    });
+
+    const validationErrors = user.validateSync();
+
+    if (validationErrors) {
+      if (avatar && fs.existsSync(avatar.path)) {
+        fs.unlinkSync(avatar.path);
+      }
+
+      const message = Object.values(validationErrors.errors).map(
+        (error) => error.message
+      );
+
+      res.status(400);
+
+      throw new Error(message.join("\n"));
+    }
+
+    if (avatar && fs.existsSync(avatar.path)) {
+      copyFileSync(avatar.path, `avatars/${avatar.filename}`);
+      fs.unlinkSync(avatar.path);
+      user.avatar = avatar.filename;
+    }
+
+    await User.register(user, password);
+
+    res.redirect("/users");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const update = async (req, res, next) => {
+  try {
+    const { firstName, lastName, nickname, email, password, avatar } =
+      getStrongParams(req);
+    const user = await findAndVerifyUser(req);
+
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.nickname = nickname;
+    user.email = email;
+
+    const validationErrors = user.validateSync();
+
+    if (validationErrors) {
+      if (avatar && fs.existsSync(avatar.path)) {
+        fs.unlinkSync(avatar.path);
+      }
+
+      const message = Object.values(validationErrors.errors).map(
+        (error) => error.message
+      );
+
+      res.status(400);
+
+      throw new Error(message.join("\n"));
+    }
+
+    if (avatar && fs.existsSync(avatar.path)) {
+      copyFileSync(avatar.path, `avatars/${avatar.filename}`);
+      fs.unlinkSync(avatar.path);
+      fs.unlinkSync(`avatars/${user.avatar}`);
+      user.avatar = avatar.filename;
+    }
+
+    if (password) {
+      await user.setPassword(password);
+    }
+
+    user.save();
+
+    res.redirect("/users");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const remove = async (req, res, next) => {
+  try {
+    const user = await findAndVerifyUser(req);
+
+    const filepath = `avatars/${user.avatar}`;
+
+    if (fs.existsSync(filepath)) {
+      fs.unlinkSync(filepath);
+    }
+
+    await User.findByIdAndDelete(req.params.id);
+
+    res.redirect("/users");
+  } catch (error) {
+    next(error);
+  }
+};
